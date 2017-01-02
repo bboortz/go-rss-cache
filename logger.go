0c527c5b5b647c6f64705c80bc612482c436a652
Benjamin Boortz <benjamin.boortz@secure.mailbox.org>
2017-01-02 12:19:02 +0100

    channel added to logging /BB

--- logger.go
+++ logger.go
@@ -8,5 +8,5 @@ import (
 var log = logging.MustGetLogger("rss-cache")
 
 func logItemAdded(s rsslib.RssItem) {
-	log.Infof("%s\titem added: %d - %s ", log.Module, s.Id, s.Title)
+	log.Infof("%s\titem added: %d - %s - %s ", log.Module, s.Id, s.Channel, s.Title)
 }
